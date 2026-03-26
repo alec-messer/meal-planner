3198f7b082bbfb233dde34260ec53936d0809fbe
Alec Messer <alec.messer@outlook.com>
2026-03-26 14:11:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -139,3 +139,242 @@ def delete_meal():
 if __name__ == '__main__':
     init_db()
     app.run(debug=True)
+
+products = {
+    # MEAT ################################
+    'Chicken Breast' : [
+        {
+            'id' : 'chicken_small',
+            'search' : 'Waitrose Slower Reared 2 Chicken Breast Fillets',
+            'size' : 380,
+            'unit' : 'grams',
+            'price' : 4.75
+        },
+        {
+            'id' : 'chicken_large',
+            'search' : 'Waitrose Slower Reared Chicken Breast Fillets',
+            'size' : 600,
+            'unit' : 'grams',
+            'price' : 5.5
+        },
+        {
+            'id' : 'chicken_extra_large',
+            'search' : 'Waitrose Slower Reared Chicken Breast Fillets - XL Pack',
+            'size' : 1200,
+            'unit' : 'grams',
+            'price' : 9.5
+        },
+    ],
+    'Beef Mince' : [
+        {
+            'id' : 'beef_mince_small',
+            'search' : 'Waitrose British Native Breed Beef Mince 12% Fat',
+            'size' : 500,
+            'unit' : 'grams',
+            'price' : 4.50
+        },
+        {
+            'id' : 'beef_mince_large',
+            'search' : 'Waitrose British Native Breeds Beef Mince 15%',
+            'size' : 750,
+            'unit' : 'grams',
+            'price' : 7
+        }
+    ],
+    'Diced Beef' : [
+        {
+            'id' : 'diced_beef',
+            'search' : 'Essential British Beef Diced Braising Steak',
+            'size' : 400,
+            'unit' : 'grams',
+            'price' : 4.5
+        }
+    ],
+    'Bacon' : [
+        {
+            'id' : 'bacon',
+            'search' : 'Waitrose 12 Made Without Nitrite Smoked Streaky Bacon Rashers',
+            'size' : 250,
+            'unit' : 'grams',
+            'price' : 4
+        }
+    ],
+    'Sausages' : [
+        {
+            'id' : 'sausages',
+            'search' : 'Waitrose 6 Cumberland Pork Sausages',
+            'size' : 400,
+            'unit' : 'grams',
+            'price' : 4
+        }
+    ],
+    'Chorizo' : [
+        {
+            'id' : 'chorizo',
+            'search' : 'Waitrose Spanish Hot & Spicy Chorizo Ring',
+            'size' : 225,
+            'unit' : 'grams',
+            'price' : 3.75
+        }
+    ],
+    # VEGETABLES ################################
+    'Broccoli' : [
+        {
+            'id' : 'broccoli',
+            'search' : 'Essential Broccoli',
+            'size' : 400,
+            'unit' : 'grams',
+            'price' : 0.96
+        }
+    ],
+    'Carrot' : [
+        {
+            'id' : 'carrot',
+            'search' : 'Essential British Loose Carrots',
+            'size' : 140,
+            'unit' : 'grams',
+            'price' : 0.1
+        }
+    ],
+    'Onion' : [
+        {
+            'id' : 'onion',
+            'search' : 'Essential Onions',
+            'size' : 150,
+            'unit' : 'grams',
+            'price' : 0.16
+        }
+    ],
+    'Celery' : [
+        {
+            'id' : 'celery',
+            'search' : 'Essential Green Celery',
+            'size' : 375,
+            'unit' : 'grams',
+            'price' : 0.9
+        }
+    ],
+    'Pepper' : [
+        {
+            'id' : 'bell_pepper',
+            'search' : 'Waitrose Red Peppers',
+            'size' : 160,
+            'unit' : 'grams',
+            'price' : 0.72
+        }
+    ],
+    'Potatoes' : [
+        {
+            'id' : 'potatoes',
+            'search' : 'Waitrose Maris Piper Potatoes',
+            'size' : 2000,
+            'unit' : 'grams',
+            'price' : 2.2
+        }
+    ],
+    'Peas' : [
+        {
+            'id' : 'peas',
+            'search' : 'Essential Frozen British Garden Peas',
+            'size' : 725,
+            'unit' : 'grams',
+            'price' : 1.25
+        }
+    ],
+    # OTHER ################################
+    'Pasta' : [
+        {
+            'id' : 'pasta',
+            'search' : 'No.1 Italian Mafaldine Pasta',
+            'size' : 500,
+            'unit' : 'grams',
+            'price' : 3
+        }
+    ],
+    'Birria Paste' : [
+        {
+            'id' : 'birria_paste',
+            'search' : 'Waitrose Birria Paste',
+            'size' : 90,
+            'unit' : 'grams',
+            'price' : 2
+        }
+    ],
+    'Spag Bol Seasoning' : [
+        {
+            'id' : 'spag_bol_paste',
+            'search' : 'Jamie Oliver Brilliant Bolognese Paste',
+            'size' : 180,
+            'unit' : 'grams',
+            'price' : 2.5
+        }
+    ],
+    'Tortilla' : [
+        {
+            'id' : 'tortilla',
+            'search' : 'Gran Luchito 10 Mexican Soft Taco Wraps',
+            'size' : 300,
+            'unit' : 'grams',
+            'price' : 2.2
+        }
+    ],
+    'Chopped Tomatoes' : [
+        {
+            'id' : 'chopped_tomatoes_small',
+            'search' : 'Waitrose Finely Chopped Italian Tomatoes',
+            'size' : 400,
+            'unit' : 'grams',
+            'price' : 0.95
+        },
+        {
+            'id' : 'chopped_tomatoes_large',
+            'search' : 'Essential Chopped Tomatoes in Natural Juice',
+            'size' : 1600,
+            'unit' : 'grams',
+            'price' : 2.4
+        }
+    ],
+    'Cheddar' : [
+        {
+            'id' : 'cheddar_small',
+            'search' : 'Cathedral City Mature Cheddar Cheese',
+            'size' : 350,
+            'unit' : 'grams',
+            'price' : 4.25
+        },
+        {
+            'id' : 'cheddar_large',
+            'search' : 'Cathedral City Mature Cheddar Cheese Large',
+            'size' : 550,
+            'unit' : 'grams',
+            'price' : 4.6
+        }
+    ],
+    'Parmesan' : [
+        {
+            'id' : 'parmesan',
+            'search' : 'Waitrose Grana Padano DOP Cheese Strength 4',
+            'size' : 175,
+            'unit' : 'grams',
+            'price' : 3.3
+        }
+    ],
+    'Eggs' : [
+        {
+            'id' : 'eggs_large',
+            'search' : 'Waitrose FR Mixed Size Eggs British Blacktail',
+            'size' : 700,
+            'unit' : 'grams',
+            'price' : 3.4
+        }
+    ],
+    'Microwave Rice' : [
+        {
+            'id' : 'microwave_rice',
+            'search' : 'Veetee Steam Filtered Sticky Rice',
+            'size' : 260,
+            'unit' : 'grams',
+            'price' : 1.2
+        }
+    ],
+}
