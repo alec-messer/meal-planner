0ea45ee568ccbea99c9d0816f2f237a6de4ab843
Alec Messer <alec.messer@outlook.com>
2026-04-14 13:50:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -516,7 +516,7 @@ def run_playwright(basket):
         page = context.new_page()
 
         # homepage
-        safe_goto(page, 'https://www.waitrose.com/')
+        safe_goto(page, 'https://www.waitrose.com/ecom/sign-in')
 
         try:
             page.locator("button:has-text('Accept')").click(timeout=3000)
